579090d7cb164997ad696d771b3778f56102a7cc
Paul Koepke <paul.koepke@gmail.com>
2023-05-21 21:23:51 -0500

    Changed service worker registration to check for updates.

--- indexScript.js
+++ indexScript.js
@@ -1,13 +1,27 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("serviceworker.js").then(
-    (registration) => {
-      console.log("Service worker registration succeeded:", registration);
-      registration.update(); // Go ahead and update the SW every time while testing.
-    },
-    (error) => {
-      console.error(`Service worker registration failed: ${error}`);
-    }
-  );
-} else {
-  console.error("Service workers are not supported.");
+const registerServiceWorker = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/serviceworker.js")
+      .then((registration) => {
+        registration.addEventListener("updatefound", () => {
+          // If updatefound is fired, it means that there's
+          // a new service worker being installed.
+          const installingWorker = registration.installing;
+          console.log(
+            "A new service worker is being installed:",
+            installingWorker
+          );
+
+          // You can listen for changes to the installing service worker's
+          // state via installingWorker.onstatechange
+        });
+      })
+      .catch((error) => {
+        console.error(`Service worker registration failed: ${error}`);
+      });
+  } else {
+    console.error("Service workers are not supported.");
+  }
+
 }
+registerServiceWorker(); // run whenever the page loads.
