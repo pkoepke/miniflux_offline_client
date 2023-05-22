56f27953e3b418d88ec4620c040c14f1e12a60c7
Paul Koepke <paul.koepke@gmail.com>
2023-05-22 08:54:57 -0500

    Service worker changes, DOCTYPE declaration

--- serviceworker.js
+++ serviceworker.js
@@ -7,7 +7,7 @@ console.log('Running serviceworker.js');
 // Once the SW is installed, cache files for offline use.
 self.addEventListener("install", (e) => {
   console.log("Service Worker Install event fired, caching files.");
-  //const contentToCache = ['index.html', 'script.js', 'styles.css', '/favicon.png']; // Array of URLs to cache. Will add all static content when released.
+  //const contentToCache = ['/miniflux_offline_client/unread/index.html', '/miniflux_offline_client/unread/styles.css', '/favicon.png']; // Array of URLs to cache. Will add all static content when released.
   const contentToCache = ['/favicon.png']; // Array of URLs to cache, currently limited to just favicon for testing purposes.
   e.waitUntil(
     (async () => {
@@ -23,13 +23,13 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`); // Attempt to serve from cache.
+      //console.log(`[Service Worker] Fetching resource: ${e.request.url}`); // Attempt to serve from cache.
       if (r) {
         return r; // Ends here if the resource is in the cache.
       }
       const response = await fetch(e.request); // If the resource wasn't found in the cache, fetch it online.
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      //console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })()
