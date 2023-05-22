6434291acba1e78524450cf877a7fdf811c6d95c
Paul Koepke <paul.koepke@gmail.com>
2023-05-21 21:55:38 -0500

    Same

--- serviceworker.js
+++ serviceworker.js
@@ -21,8 +21,8 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   console.log('Caught request: ' + e.request.url);
   console.log('e.request.url.indexOf(\'chrome - extension\' )' + e.request.url.indexOf('chrome-extension'))
-  if (e.request.url.indexOf('chrome-extension') == 0) { // Bypass cache if it's a chrome extension, which cache doesn't support.
-    (async () => { const response = await fetch(e.request); })()
+  if (e.request.url.indexOf('chrome-extension') === 0) { // Bypass cache if it's a chrome extension, which cache doesn't support.
+    (async () => { const response = await fetch(e.request); return response; })()
   }
   e.respondWith(
     (async () => {
